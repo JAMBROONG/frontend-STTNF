b65f18cd290c559e593428505e19d28c49d4d15c
Akhmad Lylana <57022642+JAMBROONG@users.noreply.github.com>
2022-03-15 21:23:25 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+/**
+ * Import Component Hello
+ */
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import Hello from "./Hello";
+const Main = () => {
+    return (
+      <main>
+        <Hello name="Aufa" />
+        <Hello name="Mikel" />
+        <Hello name="Hannah" />
+        <Hello name="Jonas" />
+        <Hello name="Martha" />
+      </main>
+    );
+  }
+
+  export default Main;
